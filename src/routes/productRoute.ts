ebd17067e8aaa82c8409f7e7c07da286a9739973
Juan Antonio <juananthonio23@gmail.com>
2024-11-10 22:11:25 +0700

    feat: change product route

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -11,8 +11,8 @@ const productRoute = new Hono();
 
 productRoute.get('/', getAllProduct);
 productRoute.get('/:id{[0-9]+}', getProductDetail);
+productRoute.post('/update/:id{[0-9]+}', updateProduct);
 productRoute.post('/', createProduct);
-productRoute.put('/:id{[0-9]+}', updateProduct);
 productRoute.delete('/:id{[0-9]+}', deleteProduct);
 
 export default productRoute;
